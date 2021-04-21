0e2bfb4178c005542b38c338af5c935fcf4a005e
Josh Levan <joshlevan@Joshs-iMac-2.local>
2021-04-21 17:30:04 -0400

    add onChange

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 
 export default function Form(){
 
+    const onChange = e => {
+        
+    }
+
     const [formName, setFormName] = useState('');
     const [formEmail, setFormEmail] = useState('');
     const [checked, setChecked] = useState(false);
@@ -10,19 +14,30 @@ export default function Form(){
         <form>
             <label>
                 Name
-                <input type="text" value={formName}/>
+                <input 
+                type="text" 
+                value={formName}
+                onChange={onChange}/>
             </label>
             <label>
                 eMail
-                <input type="text" value={formEmail}/>
+                <input 
+                type="text" 
+                value={formEmail}
+                onChange={onChange}/>
             </label>
             <label>
                 Password
-                <input type="password"/>
+                <input 
+                type="password"
+                onChange={onChange}/>
             </label>
             <label>
                 I agree to terms of service
-                <input type="checkbox" checked={checked}/>
+                <input 
+                type="checkbox" 
+                checked={checked}
+                onChange={onChange}/>
             </label>
             <button>Submit</button>
         </form>
