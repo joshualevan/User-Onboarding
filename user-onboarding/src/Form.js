19e6e2d95dda0dea925cb749008394e3d1670e42
Josh Levan <joshlevan@Joshs-iMac-2.local>
2021-04-21 17:26:11 -0400

    form state and values

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Form(){
+
+    const [formName, setFormName] = useState('');
+    const [formEmail, setFormEmail] = useState('');
+    const [checked, setChecked] = useState(false);
+
     return(
         <form>
             <label>
                 Name
-                <input type="text"/>
+                <input type="text" value={formName}/>
             </label>
             <label>
                 eMail
-                <input type="text"/>
+                <input type="text" value={formEmail}/>
             </label>
             <label>
                 Password
@@ -17,7 +22,7 @@ export default function Form(){
             </label>
             <label>
                 I agree to terms of service
-                <input type="checkbox"/>
+                <input type="checkbox" checked={checked}/>
             </label>
             <button>Submit</button>
         </form>
